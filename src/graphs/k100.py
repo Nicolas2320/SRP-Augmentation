69757f3db677f9de19c697d420207b086efef2ea
EyaAtaknit <eyaa561@gmail.com>
2026-05-12 14:51:51 +0200

    ResNet18,k20,50,100 CutMix

---
+++ src/graphs/k100.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load CSV
+df = pd.read_csv(
+    "results/metrics/cifar10_resnet18_k100_seed0_cutmix_epochs30.csv"
+)
+
+# =====================================================
+# ACCURACY GRAPH
+# =====================================================
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(
+    df["epoch"],
+    df["train_acc"],
+    label="Train Accuracy"
+)
+
+plt.plot(
+    df["epoch"],
+    df["val_acc"],
+    label="Validation Accuracy"
+)
+
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+
+plt.title(
+    "ResNet18 + CutMix on CIFAR-10 (k=100)"
+)
+
+plt.legend()
+
+plt.grid(True)
+
+plt.show()
---
+++ src/graphs/k50.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load CSV
+df = pd.read_csv(
+    "results/metrics/cifar10_resnet18_k50_seed0_cutmix_epochs30.csv"
+)
+
+# =====================================================
+# ACCURACY GRAPH
+# =====================================================
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(
+    df["epoch"],
+    df["train_acc"],
+    label="Train Accuracy"
+)
+
+plt.plot(
+    df["epoch"],
+    df["val_acc"],
+    label="Validation Accuracy"
+)
+
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+
+plt.title(
+    "ResNet18 + CutMix on CIFAR-10 (k=50)"
+)
+
+plt.legend()
+
+plt.grid(True)
+
+plt.show()
